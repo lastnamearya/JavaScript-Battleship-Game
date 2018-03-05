d701e4d63e59abb7c11d8d2cee7be4ea10550722
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-05 09:03:19 +0530

    Test Drive of our Controller's processGuess

--- battleship.js
+++ battleship.js
@@ -86,12 +86,34 @@ var model = {
 var controller = {
   guesses: 0,
 
-
   processGuess: function(guess){
-    var location = parse
-    
+    var location = parseGuess(guess);
+    console.log(location);
+
+    if(location){
+      this.guesses++;
+      var hit = model.fire(location);
+      if(hit && model.shipSunk === model.numShips) {
+        view.displayMessage('You sank all my battleship, in' + this.guesses + " guesses");
+      }
+    }
   }
-}
+};
+
+// Test Drive of our Controller's ProcessGuess
+controller.processGuess("A0");
+
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
 
 function parseGuess(guess) {
   var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
@@ -114,27 +136,3 @@ function parseGuess(guess) {
   return null;
 }
 
-// Test Drive of our parseGuess Function
-
-console.log(parseGuess("A0"));
-console.log(parseGuess("B6"));
-console.log(parseGuess("G3"));
-console.log(parseGuess("H0"));
-console.log(parseGuess("A7"));
-
-
-
-
-// // Test Drive of our Model Object
-
-// model.fire("53");
-// model.fire("06");
-// model.fire("16");
-
-// model.fire("26");
-// model.fire("34");
-// model.fire("44");
-
-// model.fire("12");
-// model.fire("11");
-// model.fire("10");
