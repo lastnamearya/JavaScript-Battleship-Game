8f90247ea1e4cad6f86a8d2aaa20dcfb1e85f4e5
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-05 07:50:17 +0530

    Model is successfuly implemented with TDD

--- battleship.js
+++ battleship.js
@@ -1,10 +1,8 @@
 // It's an MVC App
 
-// View of our App
+// View Object, responsible for the view of our Game
 
 var view = {
-  // this method takes a string message and displays it
-  // in the message display area
 
   displayMessage: function(msg) {
     var messageArea = document.getElementById('messageArea');
@@ -22,15 +20,74 @@ var view = {
   }
 }
 
-// Test Drive of our View Object
-
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-
 // Display for the user
 
 view.displayMessage('Tap tap, is this thing on?');
+
+// *************************************************************** //
+
+// Model object, which controls the state of our Game
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    {
+      locations: ["10", "20", "30"],
+      hits: ["", "", ""]
+    },
+    {
+      locations: ["32", "33", "34"],
+      hits: ["", "", ""]
+    },
+    {
+      locations: ["63", "64", "65"],
+      hits: ["", "", ""]
+    }],
+
+  fire: function(guess){
+    for(var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if(index >= 0){
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage('HIT');
+        if (this.isSunk(ship)){
+          view.displayMessage("You sank my batleship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+
+  isSunk: function(ship) {
+    for(var i = 0; i < this.shipLength; i++){
+      if(ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+// Test Drive of our Model Object
+
+model.fire("53");
+model.fire("06");
+model.fire("16");
+
+model.fire("26");
+model.fire("34");
+model.fire("44");
+
+model.fire("12");
+model.fire("11");
+model.fire("10");
